1e05d8f03b1cc01210bd47f4db2d7be685541ab1
xianxiao <xianxiao.zk@antgroup.com>
2024-01-25 10:31:01 +0800

    完善压缩url逻辑

--- RunTime/AntService/Implements/Services.cs
+++ RunTime/AntService/Implements/Services.cs
@@ -212,6 +212,10 @@ namespace Ant.Metaverse
         public string GetCompressedAvatarUrl(string url, int size = 256)
         {
             try{
+                string[] strs = url.Split("/original");
+                if(strs.Length > 2){
+                    return url;
+                }
                 string targetSize = string.Format("/{0}w", size);
                 int index = url.IndexOf("/original");
                 if(index == -1){
